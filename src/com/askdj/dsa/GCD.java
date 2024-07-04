4c7148d329b02b42a6e19a74a020052228fdf3f8
Syam Prasad <syamprasad1078@gmail.com>
2024-07-04 13:25:27 +0530

    chore: UPDATE GCD code in efficient manner.

--- src/com/askdj/dsa/GCD.java
+++ src/com/askdj/dsa/GCD.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class GCD {
 
     static int gcd(int n, int m){
+        int steps=0;
         int min = -1;
         if(m>n) min = n;
         else min = m;
@@ -13,6 +14,8 @@ public class GCD {
             if(m%i==0 && n%i==0){
                 return i;
             }
+            steps++;
+            System.out.println("steps: "+steps);
         }
         return -1;
     }
@@ -21,7 +24,37 @@ public class GCD {
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        System.out.println(gcd(n,m));
+        System.out.println(gcd(n,m)); //Brute force approach
+        System.out.println(eclidsGcd(n,m)); //Eclid's Algorithm
+        System.out.println(efficientEclids(n,m)); //eclid's algorithm
 
+    }
+
+    private static int efficientEclids(int n, int m) {
+        int steps = 0;
+        while(n!=0 && m!=0) {
+            if (m > n) m = m % n;
+            else n = n % m;
+        }
+        steps++;
+        System.out.println("Steps: "+steps);
+        if(n!=0) return n;
+        else return m;
+    }
+
+    private static int eclidsGcd(int n, int m) {
+
+        int steps = 0;
+        while(n!=m) {
+            if (m > n) {
+                m = m - n;
+            } else {
+                n = n - m;
+            }
+            steps++;
+            System.out.println("steps: "+steps);
+        }
+
+        return m;
     }
 }
