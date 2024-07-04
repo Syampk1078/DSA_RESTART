10f6071c5ab73ec8f412d594151e7be0ef48c3fa
Syam Prasad <syamprasad1078@gmail.com>
2024-07-04 08:43:52 +0530

    feat: Add TrailingZeroes of factorial finder

---
+++ src/com/askdj/dsa/TrailingZeroes.java
@@ -0,0 +1,17 @@
+package com.askdj.dsa;
+
+public class TrailingZeroes {
+    public static void main(String[] args) {
+        System.out.println(zeroes(25));
+    }
+
+    private static int zeroes(int n) {
+        int res = 0;
+        int powerOf5 = 5;
+        while(n>=powerOf5){
+            res += (n/powerOf5);
+            powerOf5 *= 5;
+        }
+        return res;
+    }
+}
