3c87433c1a1adbce23170663e5edd3b0d6953c2b
Syam Prasad <syamprasad1078@gmail.com>
2024-07-04 16:26:34 +0530

    chore: Add Prime Number checker

---
+++ src/com/askdj/dsa/PrimeNumber.java
@@ -0,0 +1,39 @@
+package com.askdj.dsa;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(isPrime(n));
+        System.out.println(efficientCheck(n));
+    }
+
+    private static boolean efficientCheck(int n) {
+        //Time Complexity = O(ROOT(N)) - more efficient
+        if(n==1) return false;
+        if(n==2 || n==3) return true;
+        if(n%2==0 || n%3==0) return false;
+
+        for(int i=5; i*i<=n; i=i+6){ //starts with 5, step over 6, check i+2 and itself
+            if(n%(i+2) ==0 || n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPrime(int n) {
+
+        if(n==1) return false;
+        for(int i=2;i<=n/2;i++){ //More efficient using root n
+            //Time complexity O(n) = n/2
+            //Time Complexity O(root(n)) = root(n)
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
